014a0eb2dddcff7b3a5228ad1722389ed54ff1d8
825814 <120297399+825814@users.noreply.github.com>
2023-01-20 12:30:17 +0000

    [finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+  get 'users/edit'
   devise_for :users
   root to: "homes#top"
   get 'homes/about',as: 'about'
@@ -7,5 +9,6 @@ Rails.application.routes.draw do
   # get 'post_images/index'
   # get 'post_images/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
