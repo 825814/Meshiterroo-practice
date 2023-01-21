eb24fe7977073309fa975bcf618788f1213f90e9
825814 <120297399+825814@users.noreply.github.com>
2023-01-21 10:57:46 +0000

    [finish]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   # get 'post_images/index'
   # get 'post_images/show'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
